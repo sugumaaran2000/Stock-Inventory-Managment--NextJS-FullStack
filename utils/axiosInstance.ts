7c600d6828ca523f6180f54a6eb9b249d66ad19f
sugumaaran2000 <78503758+sugumaaran2000@users.noreply.github.com>
2025-08-21 14:21:24 +0530

    Update axiosInstance.ts

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -24,7 +24,7 @@ import Cookies from "js-cookie";
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://stockly-inventory.vercel.app/api" // Production URL
+      ? "https://singular-pastelito-db7efa.netlify.app/api" // Production URL
       : "http://localhost:3000/api", // Localhost for development
   headers: {
     "Content-Type": "application/json",
