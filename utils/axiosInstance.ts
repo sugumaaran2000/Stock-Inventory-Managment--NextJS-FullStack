6b0e9708ff3b899ad57a3f252194e268a863527b
sugumaaran2000 <78503758+sugumaaran2000@users.noreply.github.com>
2025-08-21 16:09:47 +0530

    Update axiosInstance.ts

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -22,7 +22,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "/api"
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
